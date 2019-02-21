0909b0213c7410b81bebf824aabf3e39a8029b4c
Go Tae Geon <gtg7784@lab-c.me>
2019-02-22 03:06:02 +0900

    UPDATE android silhouette

--- src/App.js
+++ src/App.js
@@ -5,6 +5,24 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <div className="Android">
+          <div className="head"/>
+
+          <div className="bodywrap">
+          
+            <div className="realbodywrap">
+              <div className="arm leftarm"/>
+              <div className="body">
+                <div className="neck"/>
+              </div>
+              <div className="arm rightarm"/>
+            </div>
+            <div className="legwrap">
+              <div className="leg leftleg"/>
+              <div className="leg rightleg"/>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
